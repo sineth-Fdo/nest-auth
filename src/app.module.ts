1c3f91bc8bebb008c7be83f080440fcc0c495789
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-12-21 01:16:26 +0530

    feat: setup auth and create models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [ AuthModule],
+  imports: [
+      MongooseModule.forRoot('mongodb://localhost:27017/nestjs-auth'),
+      AuthModule],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+
+  // POST Signup (/auth/signup)
+  @Post('signup')
+  async signup(@Body() signupData: SignupDto) {
+    return 'Signup';
+  }
+
+  // POST Login (/auth/login)
+  // POST Refresh Token 
+
+
+
+
 }
